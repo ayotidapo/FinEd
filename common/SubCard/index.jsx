e8b479ccb148c49f5c10dd7b9f3e0c89384dde13
ayotidapo <otidapson@gmail.com>
2022-10-18 19:27:05 +0100

    dynamic fw redirect url

--- common/SubCard/index.jsx
+++ common/SubCard/index.jsx
@@ -158,7 +158,7 @@ const SubCard = (props) => {
         amount,
         user,
         plan,
-        host: global.window.location.origin,
+        host: global?.window?.location.origin,
       });
 
       setFwConfig(config);
